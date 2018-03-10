2216466b5534a748f1fce063dc83166083bcdd8b
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-03-10 18:33:54 +0900

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by 佐藤賢 on 2018/03/10.
+//  Copyright © 2018年 佐藤賢. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
