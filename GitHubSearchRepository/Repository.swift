a9f7582f8931960f24a937614d878fa3f5c60168
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-03-10 19:01:16 +0900

    first commit

--- GitHubSearchRepository/Repository.swift
+++ GitHubSearchRepository/Repository.swift
@@ -1,5 +1,5 @@
 //
-//  main.swift
+//  Repository.swift
 //  GitHubSearchRepository
 //
 //  Created by 佐藤賢 on 2018/03/10.
@@ -8,5 +8,10 @@
 
 import Foundation
 
-print("Hello, World!")
-
+struct Repository: Decodable {
+  let id: Int
+  let name: String
+  let fullName: String
+  let owner: User
+  
+}
---
+++ GitHubSearchRepository/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  GitHubSearchRepository
+//
+//  Created by 佐藤賢 on 2018/03/10.
+//  Copyright © 2018年 佐藤賢. All rights reserved.
+//
+
+import Foundation
+
+struct User: Decodable {
+  let id: Int
+  let login: String
+}
